c252930568b68ef83575e4f38af21d12391c9137
taomee517 <taomee517@qq.com>
2019-11-18 15:44:07 +0800

    加入list排序

---
+++ src/main/java/com/basic/java/jdkfeature/jdk8/stream/StreamSort.java
@@ -0,0 +1,23 @@
+package com.basic.java.jdkfeature.jdk8.stream;
+
+import com.basic.java.jdkfeature.entity.Worker;
+
+import java.util.*;
+
+public class StreamSort {
+    public static void main(String[] args) {
+        List<Worker> workers = new ArrayList<>();
+        int i = 0;
+        while (i<10){
+            i++;
+            Worker worker = new Worker();
+            worker.setName(UUID.randomUUID().toString().substring(16));
+            worker.setId(25 + new Random().nextInt(10));
+            workers.add(worker);
+        }
+
+        Collections.sort(workers,Comparator.comparingInt(Worker::getId));
+        Collections.reverse(workers);
+        workers.stream().forEach(p->System.out.println(p.getId()));
+    }
+}
