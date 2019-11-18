a258d2a85049203a8961866757348b9353c7f69e
taomee517 <taomee517@qq.com>
2019-11-18 14:54:02 +0800

    加入JDK新特性

---
+++ src/main/java/com/basic/java/jdkfeature/jdk8/CollectionStreamDemo.java
@@ -0,0 +1,36 @@
+package com.basic.java.jdkfeature.jdk8;
+
+import com.basic.java.jdkfeature.entity.Worker;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author LuoTao
+ * @email taomee517@qq.com
+ * @date  2018/12/22
+ * @time  11:14
+ */
+public class CollectionStreamDemo {
+
+    public static void main(String[] args){
+        try {
+            String text = "WEARETHECHAMPIONS";
+            List<String> arr = Arrays.asList("we","are","the","champions");
+            for (String word:arr) {
+                System.out.print(word);
+                System.out.printf(" ");
+            }
+            System.out.println("!");
+            System.out.println("终于设置成功了！");
+            Worker worker = new Worker(1001,"研发部","李月舒","测试");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getAgeByName() {
+        return null;
+    }
+}
